2c0a533997cdcd298c31bc24b165b51acd7e20f6
woukl22 <woukl22@gmail.com>
2021-10-15 00:52:24 +0000

    Add email and password input box.

--- ETTest/src/Authentication/LogIn.js
+++ ETTest/src/Authentication/LogIn.js
@@ -1,14 +1,92 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import PropTypes from 'prop-types';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-const LogIn = ({navigation}) => {
+const LogIn = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
   return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Login Screen</Text>
-      <Button title="Signup" onPress={() => navigation.navigate('SignUp')}></Button>
-    </View>
+    <KeyboardAwareScrollView
+      contentContainerStyle={{flex:1}}
+      extraScrollHeight={20}
+    >
+      <View style={{flex: 1}}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Email</Text>
+          <TextInput
+            style={styles.textInput}
+            label="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            onSubmitEditing={() => {}}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => {
+              setIsFocused(false);
+            }}
+            placeholder="sample@email.com"
+            returnKeyType="next"
+            textContentType="none" // ios only
+            underlineColorAndroid="transparent" // Android only
+          />
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            style={styles.textInput}
+            label="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            onSubmitEditing={() => {}}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => {
+              setIsFocused(false);
+            }}
+            placeholder="Password"
+            returnKeyType="done"
+            secureTextEntry={true}
+            textContentType="none" // ios only
+            underlineColorAndroid="transparent" // Android only
+          />
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   );
-}
+};
+
+LogIn.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChangeText: PropTypes.func.isRequired,
+  onSubmitEditing: PropTypes.func.isRequired,
+  onBlur: PropTypes.func,
+  placeholder: PropTypes.string,
+  isPassword: PropTypes.bool,
+  returnKeyType: PropTypes.oneOf(['done', 'next']),
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  text: {
+    fontSize: 14,
+    marginTop: 20,
+    marginLeft: 20,
+    color: '#a6a6a6',
+  },
+  textInput: {
+    placeholderTextColor: '#a6a6a6',
+    color: '#000000',
+    height: 40,
+    margin: 5,
+    marginHorizontal: 20,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default LogIn;
