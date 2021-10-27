f519b4f7e71423f85300aaef6fc6af91605fe8b8
woukl22 <woukl22@gmail.com>
2021-10-27 14:05:46 +0000

    Hide status bar in Loading screen.

--- ETTest/src/Loading.js
+++ ETTest/src/Loading.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ImageBackground } from "react-native";
+import { StatusBar } from 'expo-status-bar';
 
 const Loading = () => {
   return (
       <ImageBackground
           source={require('../assets/Loading.png')}    
           style={{width:"100%",height:"100%"}}>
+          <StatusBar hidden/>
       </ImageBackground>
   );
 }
